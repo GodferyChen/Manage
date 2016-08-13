b093a1131182543d966b0bf738e3a993444d1968
陈修宏 <chenxh.cz@gmail.com>
2016-08-13 16:49:01 +0800

    study ImageLoader add util

--- app/src/main/java/com/github/chen/manager/MainActivity.java
+++ app/src/main/java/com/github/chen/manager/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,10 +14,21 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.animation.ViewPropertyAnimation;
+import com.github.chen.library.DimenManager;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageSize;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.process.BitmapProcessor;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Transformation;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +41,70 @@ public class MainActivity extends AppCompatActivity {
         ImageView ivCircle = (ImageView) findViewById(R.id.iv_pic_circle);
         ImageView ivSquare = (ImageView) findViewById(R.id.iv_pic_square);
         ImageView ivGlide = (ImageView) findViewById(R.id.iv_pic_glide);
+        final ImageView ivLoader = (ImageView) findViewById(R.id.iv_pic_imageloder);
+        String url = "http://tnb.hanyouapp.com/upload/201607/15/1468580192589.jpg";
         Picasso.with(context)
-                .load("http://tnb.hanyouapp.com/upload/201607/15/1468580192589.jpg")
+                .load(url)
                 .centerCrop()
-                .resize(250,250)
+                .resize(250, 250)
                 .error(R.mipmap.ic_launcher)
                 .placeholder(R.mipmap.ic_launcher)
                 .transform(new CircleImageTransformation())
                 .into(ivCircle);
         Picasso.with(context)
-                .load("http://tnb.hanyouapp.com/upload/201607/15/1468580192589.jpg")
+                .load(url)
                 .centerCrop()
-                .resize(250,250)
+                .resize(250, 250)
                 .error(R.mipmap.ic_launcher)
                 .placeholder(R.mipmap.ic_launcher)
                 .into(ivSquare);
         Glide.with(context)
-                .load("http://tnb.hanyouapp.com/upload/201607/15/1468580192589.jpg")
+                .load(url)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .error(R.mipmap.ic_launcher)
                 .animate(R.anim.alpha)
                 .placeholder(R.mipmap.ic_launcher)
                 .transform(new RoundTransformation(context))
                 .into(ivGlide);
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+        ImageSize targetSize = new ImageSize((int) DimenManager.dp2px(context, 48), (int)
+                DimenManager.dp2px(context, 48));
+//        imageLoader.loadImage(url, new ImageLoadingListener() {
+//            @Override
+//            public void onLoadingStarted(String s, View view) {
+//                Log.d(TAG, "onLoadingStarted() called with: s = [" + s + "], view = [" + view + "]");
+//            }
+//
+//            @Override
+//            public void onLoadingFailed(String s, View view, FailReason failReason) {
+//                Log.d(TAG, "onLoadingFailed() called with: s = [" + s + "], view = [" + view + "], failReason = [" + failReason + "]");
+//            }
+//
+//            @Override
+//            public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+//                Log.d(TAG, "onLoadingComplete() called with: s = [" + s + "], view = [" + view + "], bitmap = [" + bitmap + "]");
+//                ivLoader.setImageBitmap(bitmap);
+//            }
+//
+//            @Override
+//            public void onLoadingCancelled(String s, View view) {
+//                Log.d(TAG, "onLoadingCancelled() called with: s = [" + s + "], view = [" + view + "]");
+//            }
+//        });
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+        imageLoader.loadImage(url, targetSize, options, new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                super.onLoadingComplete(imageUri, view, loadedImage);
+                ivLoader.setImageBitmap(loadedImage);
+            }
+        });
+
     }
 }
---
+++ library/src/main/java/com/github/chen/library/DimenManager.java
@@ -0,0 +1,43 @@
+package com.github.chen.library;
+
+
+import android.content.Context;
+import android.util.TypedValue;
+
+public class DimenManager {
+
+    // dp转像素
+    public static float dp2px(Context context, float value) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, context.getResources
+                ().getDisplayMetrics());
+    }
+
+    // sp转像素
+    public static float sp2px(Context context, float value) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, context.getResources()
+                .getDisplayMetrics());
+    }
+
+    // pt转像素
+    public static float pt2px(Context context, float value) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, value, context.getResources()
+                .getDisplayMetrics());
+    }
+
+    // 英寸转像素
+    public static float in2px(Context context, float value) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_IN, value, context.getResources
+                ().getDisplayMetrics());
+    }
+
+    // 毫米转像素
+    public static float mm2px(Context context, float value) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, value, context.getResources()
+                .getDisplayMetrics());
+    }
+
+    public static float custom(Context context, float value, int unit) {
+        return TypedValue.applyDimension(unit, value, context.getResources().getDisplayMetrics());
+    }
+
+}
