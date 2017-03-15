6bdf3b04d5251a183a44e188a79e012860c28ae6
陈修宏 <chenxh.cz@gmail.com>
2017-03-15 21:00:54 +0800

    完善进度条圆环

--- app/src/main/java/com/github/chen/manager/widget/CircleProgress.java
+++ app/src/main/java/com/github/chen/manager/widget/CircleProgress.java
@@ -1,11 +1,14 @@
 package com.github.chen.manager.widget;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -23,29 +26,37 @@ public class CircleProgress extends View {
 
     private static final String TAG = "CircleProgress";
 
-    private final int UNIT_RADIUS = 80;
-    private final int default_finished_color = Color.rgb(15, 169, 197);
-    private final int default_unfinished_color = Color.rgb(221, 221, 221);
-    private final int default_main_text_color = Color.rgb(15, 169, 197);
-    private final int default_sub_text_color = Color.rgb(221, 221, 221);
-    private final int default_max = 100;
-    private final float default_main_text_size;
-    private final float default_sub_text_size;
-    private final float default_stroke_width;
-    private final String default_main_text = "--";
-    private final String default_sub_text = "总步数";
-    private final float default_arc_angle = 360 * 0.25f;
+    private static final String INSTANCE_STATE = "saved_instance";
+    private static final String INSTANCE_STROKE_WIDTH = "stroke_width";
+    private static final String INSTANCE_UNFINISHED_STROKE_COLOR = "unfinished_stroke_color";
+    private static final String INSTANCE_FINISHED_STROKE_COLOR = "finished_stroke_color";
+    private static final String INSTANCE_MAIN_TEXT_COLOR = "main_text_color";
+    private static final String INSTANCE_SUB_TEXT_COLOR = "sub_text_color";
+    private static final String INSTANCE_MAX = "max";
+    private static final String INSTANCE_PROGRESS = "progress";
+    private static final String INSTANCE_MAIN_TEXT_SIZE = "main_text_size";
+    private static final String INSTANCE_SUB_TEXT_SIZE = "sub_text_size";
+    private static final String INSTANCE_MAIN_TEXT = "main_text";
+    private static final String INSTANCE_SUB_TEXT = "sub_text";
 
-    private Paint grayCirclePaint;
-    private Paint progressPaint;
+    private final int DEFAULT_FINISHED_COLOR = Color.rgb(15, 169, 197);
+    private final int DEFAULT_UNFINISHED_COLOR = Color.rgb(221, 221, 221);
+    private final int DEFAULT_MAIN_TEXT_COLOR = Color.rgb(15, 169, 197);
+    private final int DEFAULT_SUB_TEXT_COLOR = Color.rgb(221, 221, 221);
+    private final int DEFAULT_MAX = 100;
+    private final float DEFAULT_MAIN_TEXT_SIZE;
+    private final float DEFAULT_SUB_TEXT_SIZE;
+    private final float DEFAULT_STROKE_WIDTH;
+    private final String DEFAULT_MAIN_TEXT = "--";
+    private final String DEFAULT_SUB_TEXT = "总步数";
+    private final float DEFAULT_SWEEP_DEGREE = 360 * 0.25f;
+
+    private Paint circlePaint;
     private Paint mainTextPaint;
     private Paint subTextPaint;
     private float dp1;
-    private float sp1;
     private int width;
-    private int radius;//屏幕所能形成的最大圆的半径
-    private int innerRadius;//内圆的半径，也就是要所能看见的圆的半径
-    private RectF oval;
+    private RectF rectF = new RectF();
     private final float sweepStartDegree = 90;//起点的度数
     private float sweepDegree = 0;//滑动的度数
 
@@ -74,14 +85,10 @@ public class CircleProgress extends View {
 
         dp1 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getContext().getResources()
                 .getDisplayMetrics());
-        sp1 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 1, getContext()
-                .getResources().getDisplayMetrics());
 
-        default_main_text_size = 24 * dp1;
-        default_sub_text_size = 12 * dp1;
-        default_stroke_width = 4 * dp1;
-
-        innerRadius = (int) (dp1 * UNIT_RADIUS);
+        DEFAULT_MAIN_TEXT_SIZE = sp2px(getResources(), 18);
+        DEFAULT_SUB_TEXT_SIZE = sp2px(getResources(), 12);
+        DEFAULT_STROKE_WIDTH = dp2px(getResources(), 4);
 
         TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable
                 .CircleProgress, defStyleAttr, 0);
@@ -93,28 +100,28 @@ public class CircleProgress extends View {
 
     private void initByAttributes(TypedArray attributes) {
         finishedStrokeColor = attributes.getColor(R.styleable
-                .CircleProgress_circle_finished_color, default_finished_color);
+                .CircleProgress_circle_finished_color, DEFAULT_FINISHED_COLOR);
         unfinishedStrokeColor = attributes.getColor(R.styleable
-                .CircleProgress_circle_unfinished_color, default_unfinished_color);
+                .CircleProgress_circle_unfinished_color, DEFAULT_UNFINISHED_COLOR);
         mainTextColor = attributes.getColor(R.styleable.CircleProgress_circle_main_text_color,
-                default_main_text_color);
+                DEFAULT_MAIN_TEXT_COLOR);
         mainTextSize = attributes.getDimension(R.styleable.CircleProgress_circle_main_text_size,
-                default_main_text_size);
+                DEFAULT_MAIN_TEXT_SIZE);
         subTextColor = attributes.getColor(R.styleable.CircleProgress_circle_sub_text_color,
-                default_sub_text_color);
+                DEFAULT_SUB_TEXT_COLOR);
         subTextSize = attributes.getDimension(R.styleable.CircleProgress_circle_sub_text_size,
-                default_sub_text_size);
+                DEFAULT_SUB_TEXT_SIZE);
         sweepDegree = attributes.getFloat(R.styleable.CircleProgress_circle_angle,
-                default_arc_angle);
+                DEFAULT_SWEEP_DEGREE);
         strokeWidth = attributes.getDimension(R.styleable.CircleProgress_circle_stroke_width,
-                default_stroke_width);
-        setMax(attributes.getInteger(R.styleable.CircleProgress_circle_max,default_max));
-        setProgress(attributes.getInteger(R.styleable.CircleProgress_circle_progress,0));
+                DEFAULT_STROKE_WIDTH);
+        setMax(attributes.getInteger(R.styleable.CircleProgress_circle_max, DEFAULT_MAX));
+        setProgress(attributes.getInteger(R.styleable.CircleProgress_circle_progress, 0));
         mainText = TextUtils.isEmpty(attributes.getString(R.styleable
-                .CircleProgress_circle_main_text)) ? default_main_text : attributes.getString(R
+                .CircleProgress_circle_main_text)) ? DEFAULT_MAIN_TEXT : attributes.getString(R
                 .styleable.CircleProgress_circle_main_text);
         subText = TextUtils.isEmpty(attributes.getString(R.styleable
-                .CircleProgress_circle_sub_text)) ? default_sub_text : attributes.getString(R
+                .CircleProgress_circle_sub_text)) ? DEFAULT_SUB_TEXT : attributes.getString(R
                 .styleable.CircleProgress_circle_sub_text);
     }
 
@@ -122,14 +129,9 @@ public class CircleProgress extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
         width = MeasureSpec.getSize(widthMeasureSpec);
-        radius = width / 2;
-
         //进度条所绘制的那个圆圈
-        oval = new RectF(radius - innerRadius + progressPaint.getStrokeWidth() / dp1,
-                radius - innerRadius + progressPaint.getStrokeWidth() / dp1,
-                radius + innerRadius - progressPaint.getStrokeWidth() / dp1,
-                radius + innerRadius - progressPaint.getStrokeWidth() / dp1);
-
+        rectF.set(strokeWidth / dp1, strokeWidth / dp1, width - strokeWidth / dp1, MeasureSpec
+                .getSize(heightMeasureSpec) - strokeWidth / dp1);
     }
 
     @Override
@@ -137,22 +139,22 @@ public class CircleProgress extends View {
         super.onDraw(canvas);
 
         //画灰色的圆圈
-        canvas.drawCircle(radius, radius, innerRadius - grayCirclePaint.getStrokeWidth() / dp1,
-                grayCirclePaint);
+        circlePaint.setColor(unfinishedStrokeColor);
+        canvas.drawArc(rectF, 0, 360, false, circlePaint);
         //画进度条的圆弧
-        sweepDegree = getProgress() / getMax() * 360;
-        canvas.drawArc(oval, sweepStartDegree, sweepDegree, false, progressPaint);
+        circlePaint.setColor(finishedStrokeColor);
+        sweepDegree = ((float) getProgress() / getMax()) * 360;
+        canvas.drawArc(rectF, sweepStartDegree, sweepDegree, false, circlePaint);
         //画主要的字符
         float textHeight = mainTextPaint.descent() + mainTextPaint.ascent();
         float textBaseline = (width - textHeight) / dp1;
         canvas.drawText(mainText, (width - mainTextPaint.measureText(mainText)) / dp1,
                 textBaseline, mainTextPaint);
         //画次要的字符
-        float subTextBaseLine = width / 2 + innerRadius - (subTextPaint.descent() - subTextPaint
+        float subTextBaseLine = width - (subTextPaint.descent() - subTextPaint
                 .ascent()) / dp1;
         canvas.drawText(getSubText(), (width - subTextPaint.measureText(getSubText())) /
                 dp1, subTextBaseLine, subTextPaint);
-        System.out.println("CircleProgress.onDraw " + subTextPaint.getTextSize());
     }
 
     @Override
@@ -162,29 +164,22 @@ public class CircleProgress extends View {
     }
 
     protected void initPainters() {
-        //灰色的圆圈
-        grayCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);//抗锯齿
-        grayCirclePaint.setStyle(Paint.Style.STROKE);
-        grayCirclePaint.setColor(unfinishedStrokeColor);
-        grayCirclePaint.setStrokeWidth(dp1 * strokeWidth);
-
-        //进度条的圆圈
-        progressPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        progressPaint.setStyle(Paint.Style.STROKE);
-        progressPaint.setStrokeWidth(dp1 * strokeWidth);
-        progressPaint.setColor(finishedStrokeColor);
-        progressPaint.setStrokeCap(Paint.Cap.ROUND);//设置头尾圆润
+        //圆圈
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setStyle(Paint.Style.STROKE);
+        circlePaint.setStrokeWidth(dp1 * strokeWidth);
+        circlePaint.setColor(unfinishedStrokeColor);
+        circlePaint.setStrokeCap(Paint.Cap.ROUND);//设置头尾圆润
 
         //圆圈中央的字符
         mainTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mainTextPaint.setColor(mainTextColor);
-        mainTextPaint.setTextSize(sp1 * mainTextSize);
+        mainTextPaint.setTextSize(mainTextSize);
 
         //圆圈底部的字符
         subTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         subTextPaint.setColor(subTextColor);
-        subTextPaint.setTextSize(sp1 * subTextSize);
-        System.out.println("CircleProgress.initPainters " + subTextPaint.getTextSize());
+        subTextPaint.setTextSize(subTextSize);
     }
 
     public int getFinishedStrokeColor() {
@@ -289,5 +284,55 @@ public class CircleProgress extends View {
     public void setSubText(String subText) {
         this.subText = subText;
         this.invalidate();
+    }
+
+    public static float dp2px(Resources resources, float dp) {
+        final float scale = resources.getDisplayMetrics().density;
+        return dp * scale + 0.5f;
+    }
+
+    public static float sp2px(Resources resources, float sp) {
+        final float scale = resources.getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(INSTANCE_STATE,super.onSaveInstanceState());
+        bundle.putFloat(INSTANCE_STROKE_WIDTH,getStrokeWidth());
+        bundle.putInt(INSTANCE_UNFINISHED_STROKE_COLOR,getUnfinishedStrokeColor());
+        bundle.putInt(INSTANCE_FINISHED_STROKE_COLOR,getFinishedStrokeColor());
+        bundle.putInt(INSTANCE_MAIN_TEXT_COLOR,getMainTextColor());
+        bundle.putInt(INSTANCE_SUB_TEXT_COLOR,getSubTextColor());
+        bundle.putFloat(INSTANCE_MAIN_TEXT_SIZE,getMainTextSize());
+        bundle.putFloat(INSTANCE_SUB_TEXT_SIZE,getSubTextSize());
+        bundle.putInt(INSTANCE_MAX,getMax());
+        bundle.putInt(INSTANCE_PROGRESS,getProgress());
+        bundle.putString(INSTANCE_MAIN_TEXT,getMainText());
+        bundle.putString(INSTANCE_SUB_TEXT,getSubText());
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if(state instanceof Bundle){
+            final Bundle bundle = (Bundle) state;
+            strokeWidth = bundle.getFloat(INSTANCE_STROKE_WIDTH);
+            unfinishedStrokeColor = bundle.getInt(INSTANCE_UNFINISHED_STROKE_COLOR);
+            finishedStrokeColor = bundle.getInt(INSTANCE_FINISHED_STROKE_COLOR);
+            mainTextColor = bundle.getInt(INSTANCE_MAIN_TEXT_COLOR);
+            subTextColor = bundle.getInt(INSTANCE_SUB_TEXT_COLOR);
+            mainTextSize = bundle.getFloat(INSTANCE_MAIN_TEXT_SIZE);
+            subTextSize = bundle.getFloat(INSTANCE_SUB_TEXT_SIZE);
+            mainText = bundle.getString(INSTANCE_MAIN_TEXT);
+            subText = bundle.getString(INSTANCE_SUB_TEXT);
+            setMax(bundle.getInt(INSTANCE_MAX));
+            setProgress(bundle.getInt(INSTANCE_PROGRESS));
+            initPainters();
+            super.onRestoreInstanceState(bundle.getParcelable(INSTANCE_STATE));
+            return;
+        }
+        super.onRestoreInstanceState(state);
     }
 }
