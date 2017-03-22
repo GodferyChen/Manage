761f9b3cd3298c0a79c73d3c237a3060fb2e22fd
陈修宏 <chenxh.cz@gmail.com>
2017-03-22 20:29:25 +0800

    ，，，

--- library/src/main/java/com/github/chen/library/PackageHelper.java
+++ library/src/main/java/com/github/chen/library/PackageHelper.java
@@ -1,6 +1,7 @@
 package com.github.chen.library;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
@@ -23,6 +24,34 @@ public class PackageHelper {
             info = new PackageInfo();
         }
         return info;
+    }
+
+    public static int getVersionCode(Context context) {
+        PackageInfo info = getPackageInfo(context.getApplicationContext());
+        return info.versionCode;
+    }
+
+    public static String getVersionName(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo
+                    (context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static CharSequence getUpdateInfo(Context context) {
+        ApplicationInfo appInfo = null;
+        try {
+            appInfo = context.getPackageManager().
+                    getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String dataName = appInfo.metaData.getString("updateInfo");
+        return dataName;
     }
 
 }
