7a57c3235072d48ffb999be41fb9391033697995
陈修宏 <chenxh.cz@gmail.com>
2017-09-12 18:32:02 +0800

    add a method

--- library/src/main/java/com/github/chen/library/PackageHelper.java
+++ library/src/main/java/com/github/chen/library/PackageHelper.java
@@ -131,4 +131,21 @@ public class PackageHelper {
         return false;
     }
 
+    /**
+     * 判断手机是否ROOT
+     */
+    public static boolean isRoot() {
+        boolean root = false;
+        try {
+            if ((!new File("/system/bin/su").exists())
+                    && (!new File("/system/xbin/su").exists())) {
+                root = false;
+            } else {
+                root = true;
+            }
+        } catch (Exception e) {
+        }
+        return root;
+    }
+
 }
